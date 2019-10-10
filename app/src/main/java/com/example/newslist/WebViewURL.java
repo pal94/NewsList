f19c444b00a34aad9acf010fcf6ca954a5b72643
Pallav Jhaveri <pallavjhaveri2512@gmail.com>
2019-10-10 13:34:36 -0400

    Webview goes back to app.

--- app/src/main/java/com/example/newslist/WebViewURL.java
+++ app/src/main/java/com/example/newslist/WebViewURL.java
@@ -4,12 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewURL extends AppCompatActivity {
 
     WebView webView;
     String url;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(event.getAction() == KeyEvent.ACTION_DOWN)
+        {
+            switch (keyCode)
+            {
+                case KeyEvent.KEYCODE_BACK:
+                    if(webView.canGoBack()){
+                        webView.goBack();
+                    }
+                    else{
+                        finish();
+                    }
+                    return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +41,9 @@ public class WebViewURL extends AppCompatActivity {
 
 
         webView = findViewById(R.id.newsWebView);
+        webView.getSettings().setJavaScriptEnabled(true);
 
         webView.loadUrl(url);
+        webView.setWebViewClient(new WebViewClient());
     }
 }
